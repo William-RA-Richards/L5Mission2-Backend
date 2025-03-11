5c1dd3c9838f76502d5d39dc50f71e4a9011c29b
William Richards <williamr@missionreadyhq.com>
2025-03-11 13:03:21 +1300

    added all tests, edge cases, boundary and error handling tests. Also made it so carValue Function passes all tests

--- server.js
+++ server.js
@@ -32,19 +32,49 @@ app.get("/api/2", (req, res) => {
 //TODO William's API
 
 function carValue({ model, year }) {
-  let modelValue = 0;
-  const letterRegex = new RegExp("[a-z]");
-  for (let i = 0; i < model.length; i++) {
-    const letter = model[i].toLowerCase();
-    if (letter.match(letterRegex)) {
-      const letterValue = letter.charCodeAt(0) - 97 + 1;
-      modelValue += letterValue;
+  try {
+    //* Testing if model and year are valid inputs
+    if (model === undefined && year === undefined) {
+      throw "No Inputs Detected";
+    } else if (year === undefined) {
+      throw "No Year Input Detected";
+    } else if (model === undefined) {
+      throw "No Model Input Detected";
+    } else if (typeof year != typeof 9) {
+      throw "Invalid Data Type Input For Year";
+    } else if (typeof model != typeof "9") {
+      throw "Invalid Data Type Input For Model";
     }
+
+    //* Testing if year meets minimum and model has at least 1 letter
+    if (year < 1886) {
+      throw "Entered an invalid year";
+    } else if (model.length < 1) {
+      throw "Please enter a model type";
+    }
+
+    let modelValue = 0;
+    const letterRegex = new RegExp("[a-z]");
+
+    //* Calculating each letters worth
+    for (let i = 0; i < model.length; i++) {
+      const letter = model[i].toLowerCase();
+      if (letter.match(letterRegex)) {
+        const letterValue = letter.charCodeAt(0) - 97 + 1;
+        modelValue += letterValue;
+      }
+    }
+
+    finalValue = modelValue * 100 + year;
+
+    return { car_value: finalValue };
+  } catch (error) {
+    if (!(error instanceof Error)) {
+      error = new Error(error);
+    } else if (error instanceof TypeError) {
+    }
+    return { error_message: error.toString().split(": ").pop() };
   }
-
-  modelValue = modelValue * 100 + year;
-
-  return { car_value: modelValue };
 }
 
 app.get("/api/1", (req, res) => {
@@ -56,14 +86,14 @@ app.get("/api/1", (req, res) => {
 function discountRate() {}
 
 //* Server port details
-const PORT = 4000; // Default port to 4000 if not set in .env
-app
-  .listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-  })
-  .on("error", (err) => {
-    console.error("Server Error: ", err);
-  });
+// const PORT = 4000; // Default port to 4000 if not set in .env
+// app
+//   .listen(PORT, () => {
+//     console.log(`Server is running at http://localhost:${PORT}`);
+//   })
+//   .on("error", (err) => {
+//     console.error("Server Error: ", err);
+//   });
 
 //* Export Functions for Testing
 
--- tests/carValue.test.js
+++ tests/carValue.test.js
@@ -49,23 +49,139 @@ describe("carValue function normal tests", () => {
 });
 
 describe("carValue function edge case tests", () => {
-  test.todo("Negative year number check.");
-  test.todo("Model with numbers only check.");
-  test.todo("Empty Model check.");
-  test.todo("Pre 1886 Year check.");
+  //* Negative Year Tests
+  test("Negative year number check.", () => {
+    expect(carValue({ model: "Civic", year: -200 })).toEqual({
+      error_message: "Entered an invalid year",
+    });
+    expect(carValue({ model: "Civic", year: -1200 })).toEqual({
+      error_message: "Entered an invalid year",
+    });
+    expect(carValue({ model: "Civic", year: -2 })).toEqual({
+      error_message: "Entered an invalid year",
+    });
+  });
+
+  //* Numbers Only Model Tests
+  test("Model with numbers only check.", () => {
+    expect(carValue({ model: "23985", year: 1965 })).toEqual({
+      car_value: 1965,
+    });
+    expect(carValue({ model: "2", year: 2001 })).toEqual({
+      car_value: 2001,
+    });
+    expect(carValue({ model: "447690125", year: 2024 })).toEqual({
+      car_value: 2024,
+    });
+  });
+
+  //* Empty Model Tests
+  test("Empty Model check.", () => {
+    expect(carValue({ model: "", year: 2024 })).toEqual({
+      error_message: "Please enter a model type",
+    });
+  });
+
+  //* Pre 1886 Year Tests
+  test("Pre 1886 Year check.", () => {
+    expect(carValue({ model: "SUV", year: 0 })).toEqual({
+      error_message: "Entered an invalid year",
+    });
+    expect(carValue({ model: "Sedan", year: 35 })).toEqual({
+      error_message: "Entered an invalid year",
+    });
+    expect(carValue({ model: "Wagon", year: 365 })).toEqual({
+      error_message: "Entered an invalid year",
+    });
+    expect(carValue({ model: "Civic", year: 1689 })).toEqual({
+      error_message: "Entered an invalid year",
+    });
+    expect(carValue({ model: "Hatchback", year: 1885 })).toEqual({
+      error_message: "Entered an invalid year",
+    });
+  });
 });
 
 describe("carValue function boundary tests", () => {
-  test.todo("Long Model name check.");
-  test.todo("Short model name check.");
-  test.todo("Large year check.");
-  test.todo("Small year check.");
+  //* Long Model Tests
+  test("Long Model name check.", () => {
+    expect(
+      carValue({
+        model:
+          "Land Rover Range Rover Evoque 2.0 TD4 E-Capability 4x4 HSE Dynamic",
+        year: 2000,
+      })
+    ).toEqual({
+      car_value: 58900,
+    });
+    expect(carValue({ model: "Civic-Wagon-Sedan-SUV", year: 2023 })).toEqual({
+      car_value: 23123,
+    });
+  });
+
+  //* Short Model Tests
+  test("Short model name check.", () => {
+    expect(carValue({ model: "a", year: 2024 })).toEqual({
+      car_value: 2124,
+    });
+    expect(carValue({ model: "Z", year: 2024 })).toEqual({
+      car_value: 4624,
+    });
+    expect(carValue({ model: "yb", year: 1999 })).toEqual({
+      car_value: 4699,
+    });
+  });
+
+  //* Large Year Tests
+  test("Large year check.", () => {
+    expect(
+      carValue({
+        model: "Civic",
+        year: 546908231290548200337812345678907754882520,
+      })
+    ).toEqual({
+      car_value: 546908231290548200337812345678907754887120,
+    });
+  });
+
+  //* Minimum Year Tests
+  test("Minimum year check.", () => {
+    expect(carValue({ model: "model no. 1", year: 1886 })).toEqual({
+      car_value: 9686,
+    });
+    expect(carValue({ model: "civic", year: 1886 })).toEqual({
+      car_value: 6486,
+    });
+  });
 });
 
 describe("carValue function error handling tests", () => {
-  test.todo("No Input check.");
-  test.todo("No Year check.");
-  test.todo("No Model check.");
-  test.todo("Wrong Data Type Year check.");
-  test.todo("Wrong Data Type Model check.");
+  //* No Input Test
+  test("No Input check.", () => {
+    expect(carValue({})).toEqual({
+      error_message: "No Inputs Detected",
+    });
+  });
+
+  //* No Year Input Tests
+  test("No Year check.", () => {
+    expect(carValue({ model: "Civic" })).toEqual({
+      error_message: "No Year Input Detected",
+    });
+  });
+  test("No Model check.", () => {
+    expect(carValue({ year: 1989 })).toEqual({
+      error_message: "No Model Input Detected",
+    });
+  });
+  test("Wrong Data Type Year check.", () => {
+    expect(carValue({ model: "Civic", year: "Twenty Twenty" })).toEqual({
+      error_message: "Invalid Data Type Input For Year",
+    });
+  });
+  test("Wrong Data Type Model check.", () => {
+    expect(carValue({ model: 19, year: 1994 })).toEqual({
+      error_message: "Invalid Data Type Input For Model",
+    });
+  });
 });
